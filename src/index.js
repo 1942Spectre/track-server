3bafcad6e2d1105d997a9b98be2e3d908cab25db
Afsin1942 <afsin1942@icloud.com>
2022-02-04 02:19:20 +0300

    created track create functionality

--- src/index.js
+++ src/index.js
@@ -1,24 +1,27 @@
-require("./models/User")
-require("./models/Track")
-const express = require('express')
-const mongoose = require('mongoose')
-const authRoutes = require('./routes/authRoutes')
-const bodyParser = require('body-parser')
-const requireAuth = require('./middlewares/requireAuth')
+require("./models/User");
+require("./models/Track");
+const express = require("express");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
+const bodyParser = require("body-parser");
+const trackRoutes = require("./routes/trackRoutes");
+const requireAuth = require("./middlewares/requireAuth");
 
-const mongoUri = "mongodb+srv://appAdmin:oR5pHUDwRaEKJP3h@cluster0.wasxv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-mongoose.connect(mongoUri)
-
-mongoose.connection.on('connected' , () => console.log("Connected to mongo"))
-
-mongoose.connection.on('error' , (e) => console.log("Failed to connect to mongo \n" , e))
+const mongoUri =
+  "mongodb+srv://appAdmin:oR5pHUDwRaEKJP3h@cluster0.wasxv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+mongoose.connect(mongoUri);
+mongoose.connection.on("connected", () => console.log("Connected to mongo"));
+mongoose.connection.on("error", (e) =>
+  console.log("Failed to connect to mongo \n", e)
+);
 
 const app = express();
-app.use(bodyParser.json())
-app.use(authRoutes)
+app.use(bodyParser.json());
+app.use(authRoutes);
+app.use(trackRoutes);
 
-app.get('/',requireAuth , (req,res) => {
-    res.send("Hello World")
-} )
+app.get("/", requireAuth, (req, res) => {
+  res.send("Hello World");
+});
 
-app.listen(3000, () => console.log("listening on 3000"))
+app.listen(3000, () => console.log("listening on 3000"));
