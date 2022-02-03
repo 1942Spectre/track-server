e4f154cdefee027a016a296c603696a1329805a8
Afsin1942 <afsin1942@icloud.com>
2022-02-03 20:09:30 +0300

    added the jwt token

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const mongoose = require("mongoose")
 const User = mongoose.model("User")
+const jwt = require("jsonwebtoken")
+
 const router = express.Router()
+
 
 router.post('/signup' , async (req , res) => {
     const {email,password} = req.body
@@ -10,7 +13,8 @@ router.post('/signup' , async (req , res) => {
         const user = new User({email,password})
 
         await user.save()
-        res.send("Successfully created user")
+        const token = jwt.sign({userId : user._id} , "SECRET_WEB_TOKEN" )
+        res.send({token})
     }
     catch(e){
         return res.status(422).send(e.message)
